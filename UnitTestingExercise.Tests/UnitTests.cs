1a5ad3b2450fb138c7e6835faf21f5f7be395ba5
Hector Salas Serrano <kenkira666@gmail.com>
2023-08-01 14:11:21 -0500

    added theory

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -8,7 +8,10 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData(2, 3, 5, 10)] //Add test data <-------
+        [InlineData(2, 3, 5, 10)]
+        [InlineData(3, 3, 6, 12)]
+        [InlineData(5, 6, 9, 20)]
+        [InlineData(5, 5, 7, 17)]//Add test data <-------
         public void AddTest(int num1, int num2, int num3, int expected)
         {
             //Start Step 3 here:
@@ -28,7 +31,10 @@ namespace UnitTestingExercise.Tests
         }
 
         [Theory]
-        [InlineData(9, 5, 4)]//Add test data <-------
+        [InlineData(9, 5, 4)]
+        [InlineData(10, 5, 5)]
+        [InlineData(9, 3, 6)]
+        [InlineData(8, 5, 3)]//Add test data <-------
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
@@ -42,7 +48,10 @@ namespace UnitTestingExercise.Tests
         }
 
         [Theory]
-        [InlineData(3, 5, 15)]//Add test data <-------
+        [InlineData(3, 5, 15)]
+        [InlineData(7, 5, 35)]
+        [InlineData(6, 5, 30)]
+        [InlineData(5, 9, 45)]//Add test data <-------
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
@@ -56,7 +65,12 @@ namespace UnitTestingExercise.Tests
         }
 
         [Theory]
-        [InlineData(8, 4, 2 )]//Add test data <-------
+        [InlineData(8, 4, 2 )]
+        [InlineData(15, 5, 3)]
+        [InlineData(9, 3, 4)]
+        [InlineData(18, 3, 6)]
+        [InlineData(10, 2, 5)]
+        //Add test data <-------
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
@@ -65,6 +79,9 @@ namespace UnitTestingExercise.Tests
             var actual = calculator.DivideTest(num1, num2);
             //Assert
             Assert.Equal(expected, actual);
+
+
+
         }
 
     }
